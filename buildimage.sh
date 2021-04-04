ed1980aaa95d7e363f22454fbdbda2417248e2c1
root <root@ip-172-31-18-199.ec2.internal>
2021-04-04 17:41:36 +0000

    buildimage filr added by vinodh

---
+++ buildimage.sh
@@ -0,0 +1,12 @@
+
+#This script will build docker image
+
+
+version=$BUILD_ID
+docker build -t apprepo:$version .
+aws ecr get-login-password --region us-east-2 --profile vinodhpersonal | docker login --username AWS --password-stdin 849440486996.dkr.ecr.us-east-2.amazonaws.com
+docker tag apprepo:$version 849440486996.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
+docker push 849440486996.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
+
+echo "image successfully pushed to ECR"
+
