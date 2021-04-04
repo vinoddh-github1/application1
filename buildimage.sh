110c808880008b3f73a1a2d237d618c834cc3c2d
vinoddh-github1 <81305976+vinoddh-github1@users.noreply.github.com>
2021-04-05 00:45:14 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -4,9 +4,9 @@
 
 version=$BUILD_ID
 docker build -t apprepo:$version .
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 849440486996.dkr.ecr.us-east-2.amazonaws.com
-docker tag apprepo:$version 849440486996.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
-docker push 849440486996.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 849440486996.dkr.ecr.us-east-1.amazonaws.com
+docker tag apprepo:latest 849440486996.dkr.ecr.us-east-1.amazonaws.com/apprepo:$version
+docker push 849440486996.dkr.ecr.us-east-1.amazonaws.com/apprepo:$version
 
 echo "image successfully pushed to ECR"
 
