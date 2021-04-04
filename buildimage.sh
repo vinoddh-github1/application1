741be58d5e3f0d27e530d2b515cec9de7ee8c2dd
vinoddh-github1 <81305976+vinoddh-github1@users.noreply.github.com>
2021-04-05 00:48:20 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -3,7 +3,7 @@
 
 
 version=$BUILD_ID
-docker build -t apprepo:$version .
+docker build -t apprepo .
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 849440486996.dkr.ecr.us-east-1.amazonaws.com
 docker tag apprepo:latest 849440486996.dkr.ecr.us-east-1.amazonaws.com/apprepo:$version
 docker push 849440486996.dkr.ecr.us-east-1.amazonaws.com/apprepo:$version
